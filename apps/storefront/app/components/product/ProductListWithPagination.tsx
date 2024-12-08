898998c02b49cb8a263a5d34a882a76c9060d5e3
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-12-09 01:47:57 +0530

    Update ProductListWithPagination.tsx

--- apps/storefront/app/components/product/ProductListWithPagination.tsx
+++ apps/storefront/app/components/product/ProductListWithPagination.tsx
@@ -1,44 +1,22 @@
-import React, { useState } from 'react';
 import type { FC } from 'react';
 import { PaginationWithContext } from '@app/components/common/Pagination/pagination-with-context';
 import { type ProductListProps, ProductGrid } from '@app/components/product/ProductGrid';
 import type { PaginationConfig } from '@app/components/common/Pagination';
 import { StoreProduct } from '@medusajs/types';
 
-type ProductListWithPaginationProps = {
-  products: StoreProduct[];
-  paginationConfig: PaginationConfig;
-};
+export interface ProductListWithPaginationProps extends ProductListProps {
+  products?: StoreProduct[];
+  paginationConfig?: PaginationConfig;
+  context: string;
+}
 
 export const ProductListWithPagination: FC<ProductListWithPaginationProps> = ({
-  products,
+  context,
   paginationConfig,
-}) => {
-  const [currentProducts, setCurrentProducts] = useState(products);
-  const [offset, setOffset] = useState(paginationConfig.offset);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleLoadMore = async () => {
-    setIsLoading(true);
-
-    const response = await fetch(
-      `/products.data?limit=${paginationConfig.limit}&offset=${offset + paginationConfig.limit}`
-    );
-    const { products: newProducts } = await response.json();
-
-    setCurrentProducts([...currentProducts, ...newProducts]);
-    setOffset(offset + paginationConfig.limit);
-    setIsLoading(false);
-  };
-
-  return (
-    <div>
-      <ProductGrid products={currentProducts as ProductListProps['products']} />
-      {currentProducts.length < paginationConfig.count && (
-        <button onClick={handleLoadMore} disabled={isLoading}>
-          {isLoading ? 'Loading...' : 'Load More'}
-        </button>
-      )}
-    </div>
-  );
-};
+  ...props
+}) => (
+  <div>
+    <ProductGrid {...props} />
+    {paginationConfig && <PaginationWithContext context={context} paginationConfig={paginationConfig} />}
+  </div>
+);
